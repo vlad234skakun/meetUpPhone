7cd2bb4a83de72c913979be19ea9ed3bc5141313
Vladyslav Skakun <vskakun275l@gmail.com>
2025-04-12 19:05:26 +0200

    init

--- src/Components/Header/Button/Button.jsx
+++ src/Components/Header/Button/Button.jsx
@@ -2,9 +2,12 @@ import styles from './Button.module.css';
 
 const Button = ({children}) => {
   return (
+    <>
+    <h1></h1>
     <button >
       {children}
     </button>
+    </>
   );
 }
 export default Button;
